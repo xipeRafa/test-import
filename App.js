e79dce8909a80ad563c519839ce3bc8243bc9d56
xipeRafa <43357824+xipeRafa@users.noreply.github.com>
2021-05-26 11:54:52 -0700

    fix

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import React from 'react'
-/* import {Button} from './node_modules/parcel_rafaga/components/Button' */ 
-import Button from 'npm-test-import'
+
 
 const App = () => {
       return (
             <div>
                   <p>hola from App.js</p>
-                  <Button />
+           
             </div>
       )
 }
